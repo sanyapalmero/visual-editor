62b3406b3f05c4ad1b659938038526d4118dd2dc
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-05-14 01:11:19 +0500

    add function for upload images

--- scripts/script.js
+++ scripts/script.js
@@ -41,3 +41,45 @@ function СhangeBackground(id,color)
     	$("#textarea"+i+"").css('backgroundColor', color);
 	}
 }
+
+imagecount = 0;
+maximageelems = 3;
+function LoadImage(f) {
+	imagecount++;
+	if(imagecount <= maxtextelems)
+	{
+		var doc = document;
+		var elem = doc.createElement('div');
+		elem.setAttribute("id", "image"+imagecount+"");
+		elem.setAttribute("class", "ui-widget");
+		elem.setAttribute("style", "position: relative; width: 200px; height: 20px;");
+	    var fls = f.files;
+	    if(!fls || !fls.length || !FileReader){return;}
+	    var fr = new FileReader();
+	    fr.onload = function() {
+	        var im = new Image();
+	        im.src = this.result;
+	        elem.innerHTML = "<img id=\"dragimage"+imagecount+"\" class=\"ui-widget\" style=\"border:2px solid #000; width: 200px; height: 200px; position: relative;\" src="+im.src+">";
+	        var wrapped = doc.getElementById('editorfield');
+	        wrapped.appendChild(elem);
+	        for(i=0;i<=maximageelems;i++)
+		    {
+		    	$("#image"+i+"")
+		    	.draggable({
+		    		containment: "parent"
+		    	})
+		    	.resizable({
+			        alsoResize: "#dragimage"+i+"",
+					maxWidth: 850,
+			        maxHeight: 450,
+			        handles: "all"
+			    });
+		    }
+	    };
+	    fr.readAsDataURL(fls[0]);
+	}
+	else
+	{
+		alert("Количество картинок превышено");
+	}
+}
